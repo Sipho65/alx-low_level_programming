5707c267e4c4d15335b943dad2aa62ab4270345f
Sipho65 <siphovukeya53@gmail.com>
2023-08-04 11:56:43 -0700

    didnt

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */{
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+
